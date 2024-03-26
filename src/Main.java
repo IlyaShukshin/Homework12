5018df7e34c2cb649f971a4837619a25138c6ff9
IlyaShukshin <freevhip88@mail.ru>
2024-03-26 19:31:41 +0300

    Complete & fixed

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,12 @@ public class Main {
         System.out.println("Книга: " + book1.getName());
         System.out.println("Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
         System.out.println("Год публикации: " + book1.getPublicationYear());
-        System.out.println("Хэш код автора: " + author2.hashCode());
+        System.out.println("Хэш код автора: " + author1.hashCode());
         System.out.println();
         System.out.println("Книга: " + book2.getName());
         System.out.println("Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
         System.out.println("Год публикации: " + book2.getPublicationYear());
-        System.out.println("Хэш код автора: " + author1.hashCode());
+        System.out.println("Хэш код автора: " + author2.hashCode());
         System.out.println();
         if (author1.equals(author2)) {
             System.out.println("Автор один");
