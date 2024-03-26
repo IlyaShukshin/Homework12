fa4ef57eaf80a0ce3d0530a258fec16920c80ac3
IlyaShukshin <freevhip88@mail.ru>
2024-03-26 19:19:36 +0300

    Complete

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,20 @@ public class Main {
         Book book2 = new Book("Преступление и наказание", 1866, author2);
 
         book1.setPublicationYear(1956);
-        System.out.println(book1.getName());
+        System.out.println("Книга: " + book1.getName());
         System.out.println("Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
         System.out.println("Год публикации: " + book1.getPublicationYear());
+        System.out.println("Хэш код автора: " + author2.hashCode());
         System.out.println();
-        System.out.println(book2.getName());
+        System.out.println("Книга: " + book2.getName());
         System.out.println("Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
         System.out.println("Год публикации: " + book2.getPublicationYear());
-
+        System.out.println("Хэш код автора: " + author1.hashCode());
+        System.out.println();
+        if (author1.equals(author2)) {
+            System.out.println("Автор один");
+        } else {
+            System.out.println("Авторы разные");
+        }
     }
 }
