2c62ee5182f775d851b36708d2fe2e5441ff1bf9
IlyaShukshin <freevhip88@mail.ru>
2024-03-26 03:05:53 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author author1 = new Author("Альбер", "Камю");
+        Book book1 = new Book("Падение", 1960, author1);
+
+        Author author2 = new Author("Фёдор", "Достоевский");
+        Book book2 = new Book("Преступление и наказание", 1866, author2);
+
+        book1.setPublicationYear(1956);
+        System.out.println(book1.getName());
+        System.out.println("Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
+        System.out.println("Год публикации: " + book1.getPublicationYear());
+        System.out.println();
+        System.out.println(book2.getName());
+        System.out.println("Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
+        System.out.println("Год публикации: " + book2.getPublicationYear());
+
     }
 }
